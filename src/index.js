9dd6da6eab86a5c5dda2eb0f7261b790abff1f2d
nytheneals <nythes@gmail.com>
2018-05-21 14:54:59 -0400

    [Tables Components] - Added new files

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { render } from "react-dom";
 import "./index.css";
-import Layout from "./components/Layout";
-// import store from "./containers/store";
+import FilterableProductTable from "./components/FilterableProductTable";
 import registerServiceWorker from "./registerServiceWorker";
 
-render(<Layout />, document.getElementById("root"));
+// STATE
+const PRODUCTS = [
+	{ category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football' },
+	{ category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball' },
+	{ category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball' },
+	{ category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch' },
+	{ category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5' },
+	{ category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7' }
+];
+
+render(<FilterableProductTable products={PRODUCTS} />, document.getElementById("root"));
 registerServiceWorker();
